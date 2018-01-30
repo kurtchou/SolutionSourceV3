04a604fa6255673b63d2c46d6863ea943f52fc27
Chou <feng-ting.chou@hp.com>
2018-01-30 22:44:27 +0800

    first commit to creative

---
+++ SolutionSource/obj/Debug/Package/PackageTmp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SolutionSource.MvcApplication" Language="C#" %>
---
+++ SolutionSource/obj/Debug/Package/PackageTmp/Scripts/solutionSourceCommon.js
@@ -0,0 +1,45 @@
+﻿"use strict";
+
+//declare all global variables here, name should start with g
+var gPhoneViewFlag = 0;//0 is not phone view, 1 is phone view
+
+function ReplaceLoopUpMultiID(strArray) {
+    var arrValue = []
+    var arrLookUp = strArray.split(";#");
+    var returnStr = "";
+    for (var i = 0; i < arrLookUp.length; i++) {
+        if (!isNaN(arrLookUp[i]) == false) {
+            returnStr = returnStr + arrLookUp[i] + ", "
+        }
+    }
+    return returnStr.substring(0, returnStr.length - 2);
+}
+
+function ExpandAllAsset() {
+    expandcontract.sweepToggle("expand");
+    //added 2018-01-28
+    if (gCurrentView == 2) {//asset view customization
+        $('#divContentDetail2 .handcursor').each(function () {//count all files
+            var id = $(this).attr('id').split('-title')[0];
+            if ($(this).css('display').indexOf('none') != -1) {
+                $('#' + id).hide();
+            }
+        });
+    }
+}
+
+function ContractAllAsset() {
+    expandcontract.sweepToggle("contract");
+}
+
+function Base64EncodeString(UnencodedString) {
+    //var encodedString = btoa(encodeURIComponent(UnencodedString));
+    var encodedString = btoa(UnencodedString);
+    return encodedString;
+}
+
+function Base64DecodeString(EncodedString) {
+    //decodedString = atob(decodeURIComponent(EncodedString));
+    decodedString = atob(EncodedString);
+    return decodedString;
+}
